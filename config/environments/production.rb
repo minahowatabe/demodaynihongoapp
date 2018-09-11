cd21ae85f2ef0c306b32884e5459ea0af7202cfe
minaho_watabe <minahohk@hotmail.com>
2018-09-11 13:52:18 +0000

    herokuログインエラーのため、production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   
-  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -41,7 +40,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
